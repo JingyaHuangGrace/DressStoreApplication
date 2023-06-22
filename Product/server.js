d2d7e4cde0802733b4b0afd0ae228d51bd480aa4
Jingya Huang <jhuan180@my.centennialcollege.ca>
2023-06-22 00:08:03 -0400

    added product and category models

--- Product/server.js
+++ Product/server.js
@@ -1,9 +1,5 @@
 const express = require("express");
-
 const cors = require("cors");
-
- 
-
 const app = express();
 
 
@@ -13,7 +9,31 @@ var corsOptions = {
 
 };
 
- 
+const db = require("./app/models");
+
+db.mongoose
+
+  .connect(db.url, {
+
+    useNewUrlParser: true,
+
+    useUnifiedTopology: true
+
+  })
+
+  .then(() => {
+
+    console.log("Connected to the database!");
+
+  })
+
+  .catch(err => {
+
+    console.log("Cannot connect to the database!", err);
+
+    process.exit();
+
+  });
 
 app.use(cors(corsOptions));
 
@@ -35,7 +55,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 
-  res.json({ message: "Welcome to Dress Store application." });
+  res.json({ message: "Welcome to DressStore application." });
 
 });
 
